599b42909349db787cf8690e35e16be7157fbbb3
whiskacz <pawelblasiak1985@gmail.com>
2023-09-12 10:29:37 +0200

    redux class components with ts

---
+++ src/reducers/todos.ts
@@ -0,0 +1,15 @@
+import { Todo, Action, ActionTypes} from "../components/actions";
+
+export const todosReducer = (
+    state: Todo[] = [], 
+    action: Action
+    ) => {
+        switch(action.type) {
+            case ActionTypes.fetchTodos:
+                return action.payload;
+            case ActionTypes.deleteTodo:   
+                return state.filter((todo: Todo) => todo.id !== action.payload);
+            default: 
+                return state;
+        }
+    };
